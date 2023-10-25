260a6b64733faf68a3524cd4ade6919f8d7fb1f9
fanadape <fanadape@gmail.com>
2023-10-25 06:55:50 -0700

    chore: add base styles

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+	<header></header>
+	<div>
+		<slot />
+	</div>
+	<footer>Made by fanadape🍕</footer>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped lang="scss"></style>
